7be40ea3f4ce6aedd2886dd5eb18368ae7cb4c19
vadim <lzaitsev@gmail.com>
2026-03-03 16:17:01 +0500

    add import ORM models

--- who_am_i/core/models/__init__.py
+++ who_am_i/core/models/__init__.py
@@ -1,9 +1,20 @@
 from .base import Base
+
 from .user import UserORM
+from .quizzes import QuizORM
+from .quiz_questions import QuizQuestionORM
+from .quiz_answers import QuizAnswers
+from .answer_options import AnswerOptionORM
+from .quiz_attempts import QuizAttemptORM
 from .db_helper import db_helper
 
 __all__ = (
     'Base',
     'UserORM',
+    'QuizORM',
+    'QuizQuestionORM',
+    'QuizAnswers',
+    'AnswerOptionORM',
+    'AnswerOptionORM',
     'db_helper',
 )
