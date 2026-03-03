73a135a203c0cf92de380b377b8e7f7b1f076681
vadim <lzaitsev@gmail.com>
2026-03-03 21:25:19 +0500

    add new routers

--- who_am_i/bot/handlers/__init__.py
+++ who_am_i/bot/handlers/__init__.py
@@ -1,7 +1,11 @@
 from .start import router as start_router
+from .test import router as test_router
+from .menu import router as menu_router
 from .registration import router as registration_router
 
 all_routers = [
     start_router,
     registration_router,
+    test_router,
+    menu_router,
 ]
