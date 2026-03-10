053e15c23497596a321d006343ebbafd9999004a
vadim <lzaitsev@gmail.com>
2026-03-10 21:09:02 +0500

    add new import from init service

--- who_am_i/services/__init__.py
+++ who_am_i/services/__init__.py
@@ -1,7 +1,12 @@
 from .user_service import get_current_user, get_or_create_user, create_user
+from .quiz_service import get_quiz_by_slug, get_active_quizzes
+from .quiz_attempts_service import create_quiz_attempts
 
 __all__ = (
     'get_current_user',
     'get_or_create_user',
     'create_user',
+    'get_quiz_by_slug',
+    'get_active_quizzes',
+    'create_quiz_attempts',
 )
