4f109cab04e448fb9ce68bdc08179d6be67463b1
vadim <lzaitsev@gmail.com>
2026-03-10 21:12:48 +0500

    add a check if there are no tests

--- who_am_i/bot/handlers/show_test.py
+++ who_am_i/bot/handlers/show_test.py
@@ -2,10 +2,14 @@ from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from who_am_i.services import quiz_service
-from who_am_i.bot.handlers.keyboards import build_quizzes_keyboard
+from who_am_i.bot.keyboards import build_quizzes_keyboard
 
 
 async def render_tests(message: Message, session: AsyncSession) -> None:
     quizzes = await quiz_service.get_active_quizzes(session=session)
+    if not quizzes:
+        await message.answer('Пока нет доступных тестов')
+        return
+
     kb = build_quizzes_keyboard(quizzes=quizzes)
     await message.answer('<b>Выбери тест который хочешь пройти 🤔</b>', reply_markup=kb)
