8a84e83738fd3fdba27e6bdbc8294d226aa8b562
vadim <lzaitsev@gmail.com>
2026-03-16 19:29:32 +0500

    create answer_options_service and answer_options_repo

---
+++ who_am_i/repositories/quiz_answers_repo.py
@@ -0,0 +1,31 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from who_am_i.core.models import QuizAnswers
+
+
+async def create_quiz_answer(
+    session: AsyncSession,
+    attempt_id: int,
+    question_id: int,
+    option_id: int,
+    value: int,
+) -> QuizAnswers:
+    quiz_answer = QuizAnswers(
+        attempt_id=attempt_id,
+        question_id=question_id,
+        option_id=option_id,
+        value=value,
+    )
+    session.add(quiz_answer)
+    await session.flush()
+    return quiz_answer
+
+
+async def get_quiz_answers_by_id(
+    session: AsyncSession,
+    attempt_id: int,
+) -> list[QuizAnswers]:
+    stmt = select(QuizAnswers).where(QuizAnswers.attempt_id == attempt_id)
+    result = list((await session.scalars(stmt)).all())
+    return result
---
+++ who_am_i/services/quiz_answers_service.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from who_am_i.core.models import QuizAnswers
+from who_am_i.repositories import quiz_answers_repo
+
+
+async def create_quiz_answer(
+    session: AsyncSession,
+    attempt_id: int,
+    question_id: int,
+    option_id: int,
+    value: int,
+) -> QuizAnswers:
+    quiz_answer = await quiz_answers_repo.create_quiz_answer(
+        session=session,
+        attempt_id=attempt_id,
+        question_id=question_id,
+        option_id=option_id,
+        value=value,
+    )
+    return quiz_answer
+
+
+async def get_quiz_answers_by_id(
+    session: AsyncSession,
+    attempt_id: int,
+) -> list[QuizAnswers]:
+    quiz_answers = await quiz_answers_repo.get_quiz_answers_by_id(
+        session=session,
+        attempt_id=attempt_id,
+    )
+    return quiz_answers
