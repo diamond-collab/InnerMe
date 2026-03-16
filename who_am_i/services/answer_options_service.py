ec7e1fc1a37887226744fcff75ca31190b65bfed
vadim <lzaitsev@gmail.com>
2026-03-16 19:29:08 +0500

    create answer_options_service

---
+++ who_am_i/services/answer_options_service.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from who_am_i.core.models import AnswerOptionORM
+from who_am_i.repositories import answer_options_repo
+
+
+async def get_options_by_question_id(
+    session: AsyncSession,
+    question_id: int,
+) -> list[AnswerOptionORM]:
+    options = await answer_options_repo.get_options_by_question_id(
+        session=session,
+        question_id=question_id,
+    )
+    return options
+
+
+async def get_option_by_id(session: AsyncSession, option_id: int) -> AnswerOptionORM:
+    option = await answer_options_repo.get_option_by_id(
+        session=session,
+        option_id=option_id,
+    )
+    return option
