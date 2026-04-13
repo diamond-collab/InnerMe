d817c79da4a3bf5cbf87e67e0a08d3cf86c9da2f
vadim <lzaitsev@gmail.com>
2026-04-13 18:40:36 +0500

    Рефакторинг: привел services/__init__.py к новой структуре

--- who_am_i/services/__init__.py
+++ who_am_i/services/__init__.py
@@ -1,79 +1,99 @@
-from .user_service import get_current_user, get_or_create_user, create_user
-from .quiz_service import (
-    get_quiz_by_slug,
-    get_active_quizzes,
-    get_quiz_by_id,
-    get_all_quizzes,
-    create_quiz,
-)
-from .quiz_attempts_service import (
-    create_quiz_attempts,
-    update_quiz_attempt,
-    get_attempt_by_id,
-    get_finished_attempts_by_user_id,
-    get_finished_attempts_with_quizzes_by_user_id,
-    cancel_attempt,
-)
-from .quiz_questions_service import (
-    get_questions_by_quiz_id,
-    get_question_by_id_and_order,
-    create_questions,
-    get_max_questions_order_by_quiz_id,
-    update_question_reverse,
-    change_status_by_question_id,
-    get_question_by_question_id_and_edit_text,
-)
 from .answer_options_service import (
-    get_options_by_question_id,
     create_default_answer_options_for_questions,
+    get_option_by_id,
+    get_options_by_question_id,
 )
 from .quiz_answers_service import (
     create_quiz_answer,
-    get_quiz_answers_by_id,
     get_answer_by_attempt_and_question,
+    get_quiz_answers_by_id,
 )
-
+from .quiz_attempts_service import (
+    cancel_attempt,
+    create_quiz_attempts,
+    get_attempt_by_id,
+    get_finished_attempts_by_user_id,
+    get_finished_attempts_with_quizzes_by_user_id,
+    get_in_progress_attempt,
+    update_quiz_attempt,
+)
+from .quiz_passing_service import (
+    FinishQuizResult,
+    StartQuizResult,
+    SubmitAnswerResult,
+    finish_attempt,
+    start_quiz,
+    submit_answer,
+)
+from .quiz_questions_service import (
+    change_status_by_question_id,
+    create_questions,
+    get_max_questions_order_by_quiz_id,
+    get_question_by_id,
+    get_question_by_id_and_order,
+    get_question_by_question_id_and_edit_text,
+    get_questions_by_quiz_id,
+    update_question_reverse,
+)
+from .quiz_service import (
+    create_quiz,
+    get_active_quizzes,
+    get_all_quizzes,
+    get_quiz_by_id,
+    get_quiz_by_slug,
+)
 from .result_service import get_result
-
+from .stats_entities import CommonStats, PopularQuizStats, QuizStats
 from .stats_service import (
     get_common_stats,
-    get_quiz_stats,
-    get_quiz_result_ranges,
     get_popular_quiz_stats,
+    get_quiz_result_ranges,
+    get_quiz_stats,
 )
-
-from .stats_entities import QuizStats, CommonStats, PopularQuizStats
+from .user_service import create_user, get_current_user, get_or_create_user
 
 __all__ = (
-    'get_current_user',
-    'get_or_create_user',
-    'create_user',
-    'get_quiz_by_slug',
-    'get_quiz_by_id',
-    'get_all_quizzes',
-    'create_quiz',
+    'create_default_answer_options_for_questions',
+    'get_option_by_id',
+    'get_options_by_question_id',
+    'create_quiz_answer',
     'get_answer_by_attempt_and_question',
-    'get_active_quizzes',
+    'get_quiz_answers_by_id',
+    'cancel_attempt',
     'create_quiz_attempts',
-    'update_quiz_attempt',
     'get_attempt_by_id',
     'get_finished_attempts_by_user_id',
     'get_finished_attempts_with_quizzes_by_user_id',
-    'cancel_attempt',
-    'get_questions_by_quiz_id',
-    'get_question_by_id_and_order',
+    'get_in_progress_attempt',
+    'update_quiz_attempt',
+    'FinishQuizResult',
+    'StartQuizResult',
+    'SubmitAnswerResult',
+    'finish_attempt',
+    'start_quiz',
+    'submit_answer',
+    'change_status_by_question_id',
     'create_questions',
     'get_max_questions_order_by_quiz_id',
-    'update_question_reverse',
-    'change_status_by_question_id',
+    'get_question_by_id',
+    'get_question_by_id_and_order',
     'get_question_by_question_id_and_edit_text',
-    'get_options_by_question_id',
-    'create_default_answer_options_for_questions',
-    'create_quiz_answer',
-    'get_quiz_answers_by_id',
+    'get_questions_by_quiz_id',
+    'update_question_reverse',
+    'create_quiz',
+    'get_active_quizzes',
+    'get_all_quizzes',
+    'get_quiz_by_id',
+    'get_quiz_by_slug',
     'get_result',
+    'CommonStats',
+    'PopularQuizStats',
+    'QuizStats',
     'get_common_stats',
-    'get_quiz_stats',
-    'get_quiz_result_ranges',
     'get_popular_quiz_stats',
+    'get_quiz_result_ranges',
+    'get_quiz_stats',
+    'create_user',
+    'get_current_user',
+    'get_or_create_user',
 )
