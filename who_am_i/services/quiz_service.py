edc449fe78b133edc411c165f98640e958274b5b
vadim <lzaitsev@gmail.com>
2026-04-13 19:10:37 +0500

    Рефакторинг: привел quiz_service к читаемому виду

--- who_am_i/services/quiz_service.py
+++ who_am_i/services/quiz_service.py
@@ -1,26 +1,15 @@
-import logging
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from who_am_i.core.models import QuizORM
 from who_am_i.repositories import quiz_repo
 
 
-logger = logging.getLogger(__name__)
-
-
 async def get_active_quizzes(session: AsyncSession) -> list[QuizORM]:
-    quizzes = await quiz_repo.get_active_quizzes(session=session)
-    return quizzes
+    return await quiz_repo.get_active_quizzes(session=session)
 
 
 async def get_quiz_by_slug(session: AsyncSession, slug: str) -> QuizORM | None:
-    quiz = await quiz_repo.quiz_by_slug(session=session, slug=slug)
-    if quiz is None:
-        return None
-
-    logger.info(f'Slug_service: {quiz.slug}')
-    return quiz
+    return await quiz_repo.quiz_by_slug(session=session, slug=slug)
 
 
 async def get_quiz_by_id(session: AsyncSession, quiz_id: int) -> QuizORM | None:
@@ -48,7 +37,7 @@ async def update_quiz_title_and_description(
     text: str,
     quiz_id: int,
     field: str,
-):
+) -> QuizORM | None:
     return await quiz_repo.update_quiz_title_and_description(
         session=session,
         text=text,
