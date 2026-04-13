5fc09b84e8639c5c688dac1736371d6b9669446c
vadim <lzaitsev@gmail.com>
2026-04-13 18:26:08 +0500

    Рефакторинг: удалил устаревший result_flow

--- who_am_i/bot/flows/__init__.py
+++ who_am_i/bot/flows/__init__.py
@@ -1,6 +1,5 @@
 from .quiz_flow import start_quiz, handle_quiz_answer, restart_quiz, continue_quiz
 from .question_flow import send_quiz_question
-from .result_flow import finish_quiz_attempt
 from .progress_flow import get_attempt_or_notify
 
 __all__ = (
@@ -9,6 +8,5 @@ __all__ = (
     'restart_quiz',
     'continue_quiz',
     'send_quiz_question',
-    'finish_quiz_attempt',
     'get_attempt_or_notify',
 )
