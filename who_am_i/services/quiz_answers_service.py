01567d327ae5fffcde7483d4109265f788769d77
vadim <lzaitsev@gmail.com>
2026-04-13 18:59:41 +0500

    Рефакторинг: привел quiz_answers_service к читаемому виду

--- who_am_i/services/quiz_answers_service.py
+++ who_am_i/services/quiz_answers_service.py
@@ -11,25 +11,23 @@ async def create_quiz_answer(
     option_id: int,
     value: int,
 ) -> QuizAnswers:
-    quiz_answer = await quiz_answers_repo.create_quiz_answer(
+    return await quiz_answers_repo.create_quiz_answer(
         session=session,
         attempt_id=attempt_id,
         question_id=question_id,
         option_id=option_id,
         value=value,
     )
-    return quiz_answer
 
 
 async def get_quiz_answers_by_id(
     session: AsyncSession,
     attempt_id: int,
 ) -> list[QuizAnswers]:
-    quiz_answers = await quiz_answers_repo.get_quiz_answers_by_id(
+    return await quiz_answers_repo.get_quiz_answers_by_id(
         session=session,
         attempt_id=attempt_id,
     )
-    return quiz_answers
 
 
 async def get_answer_by_attempt_and_question(
