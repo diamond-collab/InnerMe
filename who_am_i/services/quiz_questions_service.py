99e4ea4ae283a36d2b05c54198bbb934927be7f9
vadim <lzaitsev@gmail.com>
2026-04-13 18:55:54 +0500

    Рефакторинг: привел quiz_questions_service к читаемому виду

--- who_am_i/services/quiz_questions_service.py
+++ who_am_i/services/quiz_questions_service.py
@@ -10,29 +10,32 @@ async def get_questions_by_quiz_id(
     session: AsyncSession,
     quiz_id: int,
 ) -> list[QuizQuestionORM]:
-    questions = await quiz_questions_repo.get_questions_by_quiz_id(session, quiz_id)
-
-    return questions
-
-
-async def get_question_by_id(session: AsyncSession, question_id: int) -> QuizQuestionORM:
-    question = await quiz_questions_repo.get_question_by_id(
-        session=session, question_id=question_id
+    return await quiz_questions_repo.get_questions_by_quiz_id(
+        session=session,
+        quiz_id=quiz_id,
     )
-    return question
+
+
+async def get_question_by_id(
+    session: AsyncSession,
+    question_id: int,
+) -> QuizQuestionORM | None:
+    return await quiz_questions_repo.get_question_by_id(
+        session=session,
+        question_id=question_id,
+    )
 
 
 async def get_question_by_id_and_order(
     session: AsyncSession,
     quiz_id: int,
     order: int,
-) -> QuizQuestionORM:
-    next_question = await quiz_questions_repo.get_question_by_id_and_order(
+) -> QuizQuestionORM | None:
+    return await quiz_questions_repo.get_question_by_id_and_order(
         session=session,
         quiz_id=quiz_id,
         order=order,
     )
-    return next_question
 
 
 async def create_questions(
