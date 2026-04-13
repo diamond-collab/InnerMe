f27141f112c450dfbc0d58f317b5fcf89acbe2cb
vadim <lzaitsev@gmail.com>
2026-04-13 19:32:07 +0500

    Рефакторинг: привел result_service к читаемому виду

--- who_am_i/services/result_service.py
+++ who_am_i/services/result_service.py
@@ -2,7 +2,7 @@ import random
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from who_am_i.core.models import QuizResultTextORM, QuizResultRangeORM
+from who_am_i.core.models import QuizResultRangeORM, QuizResultTextORM
 from who_am_i.repositories import result_repo
 
 
@@ -10,17 +10,17 @@ async def get_random_result_text(
     session: AsyncSession,
     range_id: int,
 ) -> tuple[QuizResultTextORM, QuizResultRangeORM] | None:
-    result_text = await result_repo.get_active_result_texts_by_range_id(
+    result_texts = await result_repo.get_active_result_texts_by_range_id(
         session=session,
         range_id=range_id,
     )
-    if not result_text:
+    if not result_texts:
         return None
 
-    if len(result_text) == 1:
-        return result_text[0]
-    else:
-        return random.choice(result_text)
+    if len(result_texts) == 1:
+        return result_texts[0]
+
+    return random.choice(result_texts)
 
 
 async def get_result(
@@ -28,15 +28,15 @@ async def get_result(
     quiz_id: int,
     result_percent: int,
 ) -> tuple[QuizResultTextORM, QuizResultRangeORM] | None:
-    quiz_result = await result_repo.get_result_range(
+    quiz_result_range = await result_repo.get_result_range(
         session=session,
         quiz_id=quiz_id,
         result_percent=result_percent,
     )
-    if not quiz_result:
+    if not quiz_result_range:
         return None
 
     return await get_random_result_text(
         session=session,
-        range_id=quiz_result.range_id,
+        range_id=quiz_result_range.range_id,
     )
